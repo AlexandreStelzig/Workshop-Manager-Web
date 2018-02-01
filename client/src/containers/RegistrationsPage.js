045be0e7289f587b684fdfee9300954a3456fd6f
Wendy <wgaul102@uottawa.ca>
2018-02-01 12:12:08 -0500

    Create navbar with routes to blank pages

---
+++ client/src/containers/RegistrationsPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class RegistrationsPage extends Component {
+  render() {
+    return (
+    	<React.Fragment>
+		    	<div>
+		    		<h1>Registration</h1>
+		    		<p>It will show all applications acording to their statuses.</p>
+		    	</div>
+    	</React.Fragment>
+   	);
+  }
+}
---
+++ client/src/containers/UsersPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class UsersPage extends Component {
+  render() {
+    return (
+    	<React.Fragment>
+		    	<div>
+		    		<h1>Users</h1>
+		    	</div>
+    	</React.Fragment>
+   	);
+  }
+}
