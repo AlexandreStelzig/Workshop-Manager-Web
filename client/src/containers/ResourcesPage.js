7e01e626be5b5d19dff11aa9d653cc06558dd990
Mathieu Perron <mperr111@uottawa.ca>
2018-02-17 15:08:44 -0500

    Add modal to add resources
    - Created modal and form to add resources
    - Adds the resource to the table
    - Currently uses react-modal

--- client/src/containers/ResourcesPage.js
+++ client/src/containers/ResourcesPage.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes} from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import PaginationFactory from 'react-bootstrap-table2-paginator';
 import {ButtonToolbar, Button} from 'react-bootstrap';
+import Modal from 'react-modal';
 
 function buttonToolbarFormatter(cell, row){
   return (
@@ -64,15 +65,36 @@ const pagination = {
   hideSizePerPage: true
 };
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
 export default class ResourcesPage extends Component {
   filteredTestResources = testResources;
-  
+  showingModal = false;
   constructor(props) {
     super(props);
     this.state = {
-      filteredTestResources: testResources
+      filteredTestResources: testResources,
+      showingModal: false,
+      resourceName: '',
+      resourceCat: '',
+      resourceQuantity: 0,
+      resourceDoubleBook: false,
+      resourceNotes: '',
     };
     this.filterResources = this.filterResources.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.addResource = this.addResource.bind(this);
   }  
 
   filterResources(event) {
@@ -88,6 +110,39 @@ export default class ResourcesPage extends Component {
     this.setState({filteredTestResources: this.filteredTestResources});
   }
 
+  openModal = () => this.setState({showingModal: true})
+  closeModal = () => this.setState({showingModal: false})
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  addResource() {
+    var newResource = {
+      id: 0, 
+      name: this.state.resourceName, 
+      category: this.state.resourceCat, 
+      quantity: this.state.resourceQuantity, 
+      doubleBooking: this.state.resourceDoubleBook ? 'Yes' : 'No',
+      notes: this.state.resourceNotes,
+    }
+    testResources.push(newResource);
+    this.setState({
+      resourceName: '',
+      resourceCat: '',
+      resourceQuantity: 0,
+      resourceDoubleBook: false,
+      resourceNotes: '',
+      showingModal:false,
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -95,7 +150,8 @@ export default class ResourcesPage extends Component {
           <h1>Resources</h1>
         </div>
         <div className='col-md-3'>
-          <input className='form-control' type='text' placeholder='Filter Resources...' id='resourceSearch' onChange={this.filterResources} />
+          <input className='form-control' type='text' placeholder='Filter Resources...' 
+          id='resourceSearch' onChange={this.filterResources} />
         </div>
         <div>
           <BootstrapTable
@@ -105,9 +161,61 @@ export default class ResourcesPage extends Component {
             striped
             pagination={PaginationFactory(pagination)}
           />
+          <Modal
+              isOpen={this.state.showingModal}
+              onRequestClose={this.closeModal}
+              style={customStyles}
+              contentLabel="Test"
+            >
+            <form>
+              <h5>Name:</h5>
+              <input
+                name='resourceName'
+                type='text'
+                value={this.state.resourceName}
+                onChange={this.handleInputChange}
+              />
+              <h5>Category:</h5>
+              <select
+                name='resourceCat'
+                value={this.state.resourceCat}
+                onChange={this.handleInputChange}
+              >
+                <option value='Transportation'>Transportation</option>
+                <option value='Box'>Box</option>
+              </select>
+              <h5>Quantity:</h5>
+              <input
+                name='resourceQuantity'
+                type='number'
+                value={this.state.resourceQuantity}
+                onChange={this.handleInputChange}
+              />
+              <h5> Allow double booking 
+              <input
+                name='resourceDoubleBook'
+                type='checkbox'
+                checked={this.state.doubleBooking}
+                onChange={this.handleInputChange}
+              />
+              </h5>
+              <h5> Notes: </h5>
+              <textarea 
+                value={this.state.resourceNotes} 
+                onChange={this.handleInputChange} 
+                name='resourceNotes'
+              />
+              <br/>
+              <br/>
+              <ButtonToolbar>
+                <Button bsStyle='primary' onClick={this.addResource}>Save</Button>
+                <Button bsStyle='danger' onClick={this.closeModal}>Cancel</Button>
+              </ButtonToolbar>
+            </form>
+          </Modal>
         </div>
         <div>
-          <Button bsStyle='primary'>Add Resource</Button>
+          <Button bsStyle='primary' onClick={this.openModal}> Add Resource</Button>
         </div>
       </React.Fragment>
     );
