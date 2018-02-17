3c62caccb9567204d2d2c1c9ad55bd69c3ff0b71
Nicolas <nicolas.malo.info@gmail.com>
2018-02-17 18:45:25 -0500

    Make button group change view and implement fist version of week view

--- client/src/components/calendar/WeekView.js
+++ client/src/components/calendar/WeekView.js
@@ -1,36 +1,29 @@
 import React, { Component } from 'react';
-import SimpleDropdown from '../editors/SimpleDropdown';
+import CalendarGrid from './CalendarGrid';
 
 export default class WeekView extends Component {
   render() {
     const resources = [
-      { id: 0, name: 'Van' },
-      { id: 1, name: 'Instructor' },
-      { id: 2, name: 'Bin' },
+      'Van 1',
+      'Van 2',
+      'Van 3',
+      'Van 4',
+      'On Campus',
+    ];
+
+    const days = [
+      'Monday Jan 1st',
+      'Tuesday Jan 2nd',
+      'Wednesday Jan 3rd',
+      'Thursday Jan 4th',
+      'Friday Jan 5th',
+      'Saturday Jan 6th',
+      'Sunday Jan 7th',
     ];
 
     return (
       <div>
-        <h2>Week view</h2>
-        <div className="form-inline">
-          <div className="col-md-4">
-            <input type="date" className="form-control" />
-            <SimpleDropdown items={resources} />
-          </div>
-          <div className="col-md-4 offset-md-4">
-            <div className="btn-group btn-group-toggle float-right" data-toggle="buttons">
-              <label htmlFor="option1" className="btn btn-secondary">
-                <input type="radio" name="options" id="option1" autoComplete="off" /> Day
-              </label>
-              <label htmlFor="option2" className="btn btn-secondary active">
-                <input type="radio" name="options" id="option2" autoComplete="off" defaultChecked /> Week
-              </label>
-              <label htmlFor="option3" className="btn btn-secondary">
-                <input type="radio" name="options" id="option3" autoComplete="off" /> Month
-              </label>
-            </div>
-          </div>
-        </div>
+        <CalendarGrid rows={resources} cols={days} />
       </div>
     );
   }
--- client/src/containers/CalendarPage.js
+++ client/src/containers/CalendarPage.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
 import WeekView from '../components/calendar/WeekView';
-
+import SimpleDropdown from '../components/editors/SimpleDropdown';
 
 export default class CalendarPage extends Component {
+  constructor() {
+    super();
+    this.state = { viewMode: 'Week' };
+    this.handleViewModeChange = this.handleViewModeChange.bind(this);
+  }
+
+  handleViewModeChange(e) {
+    this.setState({
+      viewMode: e.target.value,
+    });
+  }
+
+  renderGroupButton() {
+    return (
+      <div className="btn-group btn-group-toggle float-right">
+        <label htmlFor="op1" className={`btn btn-secondary ${this.state.viewMode === 'Day' ? ' active' : ''}`} >
+          <input id="op1" type="radio" name="viewModeRadio" value="Day" onChange={this.handleViewModeChange} /> Day
+        </label>
+        <label htmlFor="op2" className={`btn btn-secondary ${this.state.viewMode === 'Week' ? ' active' : ''}`}>
+          <input id="op2" type="radio" name="viewModeRadio" value="Week" onChange={this.handleViewModeChange} /> Week
+        </label>
+        <label htmlFor="op3" className={`btn btn-secondary ${this.state.viewMode === 'Month' ? ' active' : ''}`}>
+          <input id="op3" type="radio" name="viewModeRadio" value="Month" onChange={this.handleViewModeChange} /> Month
+        </label>
+      </div>);
+  }
+
   render() {
+    const resources = [
+      { id: 0, name: 'Van' },
+      { id: 1, name: 'Instructor' },
+      { id: 2, name: 'Bin' },
+    ];
     return (
       <React.Fragment>
         <div>
           <h1>Calendar</h1>
-          <WeekView />
+          <h2>{this.state.viewMode} view</h2>
+          <div className="form-inline">
+            <div className="col-md-4">
+              <input type="date" className="form-control" />
+              <SimpleDropdown items={resources} />
+            </div>
+            <div className="offset-md-4 col-md-4">
+              {this.renderGroupButton()}
+            </div>
+          </div>
+          {this.state.viewMode === 'Week' && <WeekView />}
         </div>
       </React.Fragment>
     );
