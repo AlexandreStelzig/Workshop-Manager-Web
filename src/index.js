d91f666e8c5bc27db9dd5cff142014c5e8b9d2e6
Nicolas <nicolas.malo.info@gmail.com>
2018-03-08 20:57:14 -0500

    Detach create-react-app and use webpack and babel directly. Remove client folder and bring its src folder to the root.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import RegisterServiceWorker from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min';
 import '../node_modules/react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 
 
 ReactDOM.render(
-  <React.Fragment>
     <div className="container">
-      <App />
-    </div>
-  </React.Fragment>,
+      <App \>
+    </div>,
   document.getElementById('root'),
 );
-RegisterServiceWorker();
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new OpenBrowserPlugin({ url: 'http://localhost:8080' })
+  ]
+};
