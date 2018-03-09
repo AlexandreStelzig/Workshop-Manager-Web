033d3d204d981c1fd11a39fee385a7ebd5faabd9
Nicolas <nicolas.malo.info@gmail.com>
2018-03-08 21:38:04 -0500

    Install bootstrap 3 with react-bootstrap. Add webpack configuration rules for css and images/fonts.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min';
-import '../node_modules/react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
-
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-    <div className="container">
-      <App \>
-    </div>,
+  <div className="container">
+    <App />
+  </div>,
   document.getElementById('root'),
 );
