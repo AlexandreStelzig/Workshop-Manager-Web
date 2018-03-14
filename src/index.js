e657177ec3c637c4aff520be7d366a3c326c0186
Nicolas <nicolas.malo.info@gmail.com>
2018-03-14 16:04:33 -0400

    Fixes #85 . Webpack required offline-plugin to work without internet.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+
+OfflinePluginRuntime.install();
 
 ReactDOM.render(
   <div className="container">
