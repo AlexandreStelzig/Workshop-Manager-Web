983500d718fd341a23cacbdb1ddae65bc23db449
Wendy <wgaul102@uottawa.ca>
2018-03-15 11:18:03 -0400

    Fix table for bs3. This closes issue #80.

--- src/containers/RegistrationDetailPage.jsx
+++ src/containers/RegistrationDetailPage.jsx
@@ -1,4 +1,4 @@
-import { ButtonToolbar, Button, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 import React, { Component } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
@@ -25,7 +25,7 @@ const workshopsDetail = [
     price: '145',
     time: '8:00',
     grade: '5',
-    language: 'Fr',
+    language: 'French',
     teacher: 'Hélène Margueritte',
     contact: 'simon123@gmail.com',
     boys: '12',
@@ -39,7 +39,7 @@ const workshopsDetail = [
     price: '80',
     time: '9:00',
     grade: '7',
-    language: 'En',
+    language: 'English',
     teacher: 'Alfred Glouton',
     contact: 'alfred@gmail.com',
     boys: '12',
@@ -49,11 +49,52 @@ const workshopsDetail = [
   },
 ];
 
+const workshopsDetail2 = [
+  {
+    id: 0,
+    workshop: 'Arduino',
+    price: '145',
+    time: '8:00',
+    grade: '5',
+    language: 'French',
+    teacher: 'Hélène Margueritte',
+    contact: 'simon123@gmail.com',
+    boys: '12',
+    girls: '13',
+    instructor: 'Alexia Girouard',
+    volunteer: '',
+  },
+  {
+    id: 1,
+    workshop: 'Electricity',
+    price: '80',
+    time: '9:00',
+    grade: '7',
+    language: 'English',
+    teacher: 'Alfred Glouton',
+    contact: 'alfred@gmail.com',
+    boys: '12',
+    girls: '13',
+    instructor: 'Félicité Black',
+    volunteer: 'Alexia Girouard',
+  },
+];
+
 const cellEditProperties = {
   mode: 'click',
   blurToSave: true,
 };
-const listLanguages = ['En', 'Fr'];
+const listLanguages = ['English', 'French'];
+const listWorkshops = ['3D Printing', 'Beebot', 'Electricity', 'Laser Cutting', 'Arduino'];
+const listInstructors = ['Simon François', 'Alexia Girouard', 'Félicité Black'];
+
+function quantityValidator(value) {
+  const nan = Number.isNaN(parseInt(value, 10));
+  if (nan) {
+    return 'Quantity must be a number.';
+  }
+  return true;
+}
 
 export default class RegistrationDetailPage extends Component {
   constructor() {
@@ -195,15 +236,23 @@ export default class RegistrationDetailPage extends Component {
               <label className="control-label float-right">Transport: </label>
             </div>
             <div className="col-md-3">
-              <select className="selectpicker form-control">
-                <option>Not selected</option>
-                <option>Van 1</option>
-                <option>Van 2</option>
-                <option>Van 3</option>
-                <option>Van 4</option>
-                <option>Van 5</option>
-                <option>On Campus</option>
-              </select>
+              <FormGroup controlId="formControlsSelect">
+                <ControlLabel>Filter by statuses</ControlLabel>
+                <FormControl
+                  onChange={this.onSelectChange}
+                  inputRef={(inputStatus) => { this.inputStatus = inputStatus; }}
+                  componentClass="select"
+                  placeholder="select"
+                >
+                  <option value="0">Not selected</option>
+                  <option value="1">Van 1</option>
+                  <option value="2">Van 2</option>
+                  <option value="3">Van 3</option>
+                  <option value="4">Van 4</option>
+                  <option value="5">Van 5</option>
+                  <option value="6">On Campus</option>
+                </FormControl>
+              </FormGroup>
             </div>
           </div>
         </section>
@@ -216,17 +265,17 @@ export default class RegistrationDetailPage extends Component {
             cellEdit={cellEditProperties}
           >
             <TableHeaderColumn dataField="id" isKey hidden>Id</TableHeaderColumn>
-            <TableHeaderColumn dataField="workshop" >Workshop</TableHeaderColumn>
-            <TableHeaderColumn dataField="price" >Price</TableHeaderColumn>
+            <TableHeaderColumn dataField="workshop" editable={{ type: 'select', options: { values: listWorkshops } }}>Workshop</TableHeaderColumn>
+            <TableHeaderColumn dataField="price" editable={{ validator: quantityValidator }}>Price</TableHeaderColumn>
             <TableHeaderColumn dataField="time" >Time</TableHeaderColumn>
             <TableHeaderColumn dataField="grade" >Grade</TableHeaderColumn>
             <TableHeaderColumn dataField="language" editable={{ type: 'select', options: { values: listLanguages } }}>Language</TableHeaderColumn>
             <TableHeaderColumn dataField="teacher" >Teacher</TableHeaderColumn>
             <TableHeaderColumn dataField="contact" >Contact</TableHeaderColumn>
-            <TableHeaderColumn dataField="boys" >Boys</TableHeaderColumn>
-            <TableHeaderColumn dataField="girls" >Girls</TableHeaderColumn>
-            <TableHeaderColumn dataField="instructor" >instructor</TableHeaderColumn>
-            <TableHeaderColumn dataField="volunteer" >volunteer</TableHeaderColumn>
+            <TableHeaderColumn dataField="boys" editable={{ validator: quantityValidator }}>Boys</TableHeaderColumn>
+            <TableHeaderColumn dataField="girls" editable={{ validator: quantityValidator }}>Girls</TableHeaderColumn>
+            <TableHeaderColumn dataField="instructor" editable={{ type: 'select', options: { values: listInstructors } }}>instructor</TableHeaderColumn>
+            <TableHeaderColumn dataField="volunteer" editable={{ type: 'select', options: { values: listInstructors } }}>volunteer</TableHeaderColumn>
           </BootstrapTable>
         </div>
 
@@ -283,23 +332,24 @@ export default class RegistrationDetailPage extends Component {
         </section>
         <div>
           <BootstrapTable
-            data={workshopsDetail}
+            data={workshopsDetail2}
             hover
             striped
             condensed
+            cellEdit={cellEditProperties}
           >
             <TableHeaderColumn dataField="id" isKey hidden>Id</TableHeaderColumn>
-            <TableHeaderColumn dataField="workshop" >Workshop</TableHeaderColumn>
-            <TableHeaderColumn dataField="price" >Price</TableHeaderColumn>
+            <TableHeaderColumn dataField="workshop" editable={{ type: 'select', options: { values: listWorkshops } }}>Workshop</TableHeaderColumn>
+            <TableHeaderColumn dataField="price" editable={{ validator: quantityValidator }}>Price</TableHeaderColumn>
             <TableHeaderColumn dataField="time" >Time</TableHeaderColumn>
             <TableHeaderColumn dataField="grade" >Grade</TableHeaderColumn>
-            <TableHeaderColumn dataField="language" >Language</TableHeaderColumn>
+            <TableHeaderColumn dataField="language" editable={{ type: 'select', options: { values: listLanguages } }}>Language</TableHeaderColumn>
             <TableHeaderColumn dataField="teacher" >Teacher</TableHeaderColumn>
             <TableHeaderColumn dataField="contact" >Contact</TableHeaderColumn>
-            <TableHeaderColumn dataField="boys" >Boys</TableHeaderColumn>
-            <TableHeaderColumn dataField="girls" >Girls</TableHeaderColumn>
-            <TableHeaderColumn dataField="instructor" >instructor</TableHeaderColumn>
-            <TableHeaderColumn dataField="volunteer" >volunteer</TableHeaderColumn>
+            <TableHeaderColumn dataField="boys" editable={{ validator: quantityValidator }}>Boys</TableHeaderColumn>
+            <TableHeaderColumn dataField="girls" editable={{ validator: quantityValidator }}>Girls</TableHeaderColumn>
+            <TableHeaderColumn dataField="instructor" editable={{ type: 'select', options: { values: listInstructors } }}>instructor</TableHeaderColumn>
+            <TableHeaderColumn dataField="volunteer" editable={{ type: 'select', options: { values: listInstructors } }}>volunteer</TableHeaderColumn>
           </BootstrapTable>
         </div>
         <button type="button" className="btn btn-primary">+ Add another day</button>
@@ -308,128 +358,3 @@ export default class RegistrationDetailPage extends Component {
     );
   }
 }
-
-/* eslint max-len: 0 */
-/* eslint no-unused-vars: 0 */
-/*
-import React from 'react';
-import PropTypes from 'prop-types';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-
-const products = [];
-
-function addProducts(quantity) {
-  const startId = products.length;
-  for (let i = 0; i < quantity; i++) {
-    const id = startId + i;
-    products.push({
-      id: id,
-      name: 'Item name ' + id,
-      isInStock: (i % 3 === 0) ? 'yes' : 'no'
-    });
-  }
-}
-
-addProducts(5);
-
-class CheckboxFilter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.filter = this.filter.bind(this);
-    this.isFiltered = this.isFiltered.bind(this);
-  }
-
-  filter(event) {
-    if (this.refs.nokCheckbox.checked && this.refs.okCheckbox.checked) {
-      // all checkboxes are checked means we want to remove the filter for this column
-      this.props.filterHandler();
-    } else {
-      this.props.filterHandler({ callback: this.isFiltered });
-    }
-  }
-
-  isFiltered(targetValue) {
-    if (targetValue === 'no') {
-      return (this.refs.nokCheckbox.checked);
-    } else {
-      return (this.refs.okCheckbox.checked);
-    }
-  }
-
-  cleanFiltered() {
-    this.refs.okCheckbox.checked = true;
-    this.refs.nokCheckbox.checked = true;
-    this.props.filterHandler();
-  }
-
-  render() {
-    return (
-      <div>
-        <input ref='okCheckbox' type='checkbox' className='filter' onChange={ this.filter } defaultChecked={ true } /><label>{ this.props.textOK }</label>
-        <input ref='nokCheckbox' type='checkbox' className='filter' onChange={ this.filter } defaultChecked={ true } style={ { marginLeft: 30 + 'px' } } /><label>{ this.props.textNOK }</label>
-      </div>
-    );
-  }
-}
-
-CheckboxFilter.propTypes = {
-  filterHandler: PropTypes.func.isRequired,
-  textOK: PropTypes.string,
-  textNOK: PropTypes.string
-};
-
-CheckboxFilter.defaultProps = {
-  textOK: 'OK',
-  textNOK: 'Not OK'
-};
-
-function getCustomFilter(filterHandler, customFilterParameters) {
-  return (
-    <CheckboxFilter 
-      filterHandler={ filterHandler } 
-      textOK={ customFilterParameters.textOK } textNOK={ customFilterParameters.textNOK } />
-  );
-}
-
-export default class CustomFilter extends React.Component {
-  handleClick = () => {
-    this.refs.isInStock.cleanFiltered();
-  }
-
-  render() {
-    return (
-
-      <div>
-      <div className="col-md-3">
-            <FormGroup controlId="formControlsSelect">
-              <ControlLabel>Filter by statuses</ControlLabel>
-              <FormControl
-                ref='inputStatus'
-                onChange={this.onPickColor.bind(this)}
-                inputRef={ inputStatus => this.inputStatus=inputStatus }
-                componentClass="select"
-                placeholder="select"
-              >
-                <option value="all">All Statuses</option>
-                <option value="new">New</option>
-                <option value="sent">Sent</option>
-                <option value="conflict">Conflict</option>
-                <option value="confirmed">Confirmed</option>
-                <option value="cancelled">Cancelled</option>
-                <option value="consent">Consent</option>
-                <option value="paid">Paid</option>
-                <option value="unpayed">Unpayed</option>
-              </FormControl>
-            </FormGroup>
-          </div>
-        <button className='btn btn-default' onClick={ this.handleClick }>Clear Filter</button>
-        <BootstrapTable data={ products }>
-          <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
-          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-          <TableHeaderColumn ref='isInStock' dataField='isInStock' filter={ { type: 'CustomFilter', getElement: getCustomFilter, customFilterParameters: { textOK: 'yes', textNOK: 'no' } } }>Product Is In Stock</TableHeaderColumn>
-        </BootstrapTable>
-      </div>
-      
-    );
-  }
-}*/
