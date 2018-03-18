1f12bf28346efe61ad57df2dc69677efd6855408
Nicolas <nicolas.malo.info@gmail.com>
2018-03-18 00:22:37 -0400

    Create initial Webpack production config and add build command. Fixes #96

---
+++ webpack.prod.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index.jsx',
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+};
