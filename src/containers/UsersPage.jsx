3eade0ccfcf21c760f7ae19c9eb2107188914837
Alexandre Stelzig <Alexandre.Stelzig@gmail.com>
2018-03-27 21:43:53 +0900

    Update Resources and users data to test data

--- src/containers/UsersPage.jsx
+++ src/containers/UsersPage.jsx
@@ -2,35 +2,17 @@ import React, { Component } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 const languageTypes = ['English', 'French', 'Bilingual'];
-const userTypes = ['Instructor', 'Admin', 'Super Admin'];
+const userTypes = ['Instructor', 'Administrator', 'Super Administrator'];
 const licenseTypes = ['None', 'G', 'G2'];
 const statusTypes = ['Active', 'Inactive'];
 
 const users = [
   {
-    id: 0, userName: 'user0', type: 'Instructor', fullName: 'Reene Pauleen', status: 'Active', language: 'French', license: 'None',
+    id: 0, userName: 'awalker', type: 'Administrator', fullName: 'Allen Walker', status: 'Active', language: 'Bilingual', license: 'G2',
   }, {
-    id: 1, userName: 'user1', type: 'Instructor', fullName: 'Talon Ian', status: 'Active', language: 'English', license: 'G',
+    id: 1, userName: 'llee', type: 'Instructor', fullName: 'Lenalee Lee', status: 'Active', language: 'English', license: 'G',
   }, {
-    id: 2, userName: 'user2', type: 'Instructor', fullName: 'Savanna Vinnie', status: 'Active', language: 'English', license: 'G2',
-  }, {
-    id: 3, userName: 'user3', type: 'Instructor', fullName: 'Sebastian Jem', status: 'Inactive', language: 'English', license: 'G',
-  }, {
-    id: 4, userName: 'user2', type: 'Instructor', fullName: 'Lizzie Brendon', status: 'Active', language: 'English', license: 'None',
-  }, {
-    id: 5, userName: 'user3', type: 'Instructor', fullName: 'Domenic Joni', status: 'Inactive', language: 'English', license: 'G',
-  }, {
-    id: 6, userName: 'user2', type: 'Instructor', fullName: 'Dane Lizzy', status: 'Active', language: 'Bilingual', license: 'G2',
-  }, {
-    id: 7, userName: 'user3', type: 'Instructor', fullName: 'Jared Fredrick', status: 'Inactive', language: 'Bilingual', license: 'G',
-  }, {
-    id: 8, userName: 'user2', type: 'Instructor', fullName: 'Laz Rozanne', status: 'Active', language: 'French', license: 'None',
-  }, {
-    id: 9, userName: 'user3', type: 'Instructor', fullName: 'Laurelle Eugene', status: 'Inactive', language: 'Bilingual', license: 'None',
-  }, {
-    id: 10, userName: 'user3', type: 'Instructor', fullName: 'Jerrie Christobel', status: 'Inactive', language: 'French', license: 'G2',
-  }, {
-    id: 11, userName: 'user4', type: 'Admin', fullName: 'Sunshine Jonette', status: 'Active', language: 'French', license: 'None',
+    id: 2, userName: 'ykanda', type: 'Super Administrator', fullName: 'Yu Kanda', status: 'Active', language: 'French', license: 'None',
   },
 ];
 
