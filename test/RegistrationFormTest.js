ed6f87366d1db565ac397ac637ef972c60953d5a
Mathieu Perron <mperr111@uottawa.ca>
2018-05-06 22:36:10 -0400

    Change RegistrationFormTest to reflect form split
    - Made changes to reflect form split changes
    - More tests will be added

--- test/RegistrationFormTest.js
+++ test/RegistrationFormTest.js
@@ -11,33 +11,12 @@ describe('Registration form', function() {
   it ('renders', () => {
     shallow(<RegistrationForm />);
   });
-  it ('contains a Grid component', () => {
-    const wrapper = shallow(<RegistrationForm />);
-    expect(wrapper.find('Grid')).to.have.length(1);
-  });
-  it ('contains a form', () => {
-    const wrapper = mount(<RegistrationForm />);
-    expect(wrapper.find('form')).to.have.length(1);
-  });
-  it ('contains 27 form control components', () => {
-    const wrapper = shallow(<RegistrationForm />);
-    expect(wrapper.find('FormControl')).to.have.length(27);
-  });
   it ('starts on page 1', () => {
     const wrapper = shallow(<RegistrationForm />);
     expect(wrapper.state('pageKey')).to.equal(1);
   });
-  it ('has a contact email input', () => {
-    const wrapper = shallow(<RegistrationForm />);
-    expect(wrapper.find('[name="contactEmail"]')).to.have.length(1);
-  });
-  it ('has a next button for page one', () => {
-    const wrapper = shallow(<RegistrationForm />);
-    expect(wrapper.find('[name="pageOneNext"]')).to.have.length(1);
-  });
-  it ('changes to page 2 when next is clicked', () => {
-    const wrapper = shallow(<RegistrationForm />);
-    wrapper.find('[name="pageOneNext"]').simulate('click');
-    expect(wrapper.state('pageKey')).to.equal(2);
+  it ('contains GeneralInformation on page 1', () => {
+     const wrapper = mount(<RegistrationForm/>);
+     expect(wrapper.find('GeneralInformation')).to.have.length(1);
   });
 });
