790de0b5512776c6365489a1672d2571fab90a04
Nicolas <nicolas.malo.info@gmail.com>
2018-06-17 13:59:11 -0400

    Rename registration/submit-registration to registration/submit #152

--- src/services/RegistrationService.js
+++ src/services/RegistrationService.js
@@ -2,6 +2,6 @@ import BaseService from './BaseService';
 
 export default class RegistrationService extends BaseService {
   static submitRegistration(registrationValues) {
-    return super.post('registration/submit-registration', registrationValues).then(res => res.data);
+    return super.post('registration/submit', registrationValues).then(res => res.data);
   }
 }
