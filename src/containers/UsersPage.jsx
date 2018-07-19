4c95a86ffc2ce398d220a9d9ae229868f505ccd4
Alexandre Stelzig <Alexandre.Stelzig@gmail.com>
2018-07-19 21:07:12 +0900

    Add user service for user apis, connect the user page with the api as well as add validation to when creating/updating an user

--- src/containers/UsersPage.jsx
+++ src/containers/UsersPage.jsx
@@ -7,41 +7,112 @@ const userTypes = ['Instructor', 'Administrator', 'Super Administrator'];
 const licenseTypes = ['None', 'G', 'G2'];
 const statusTypes = ['Active', 'Inactive'];
 
-var users = [
-  // {
-  //   id: 0, userName: 'awalker', type: 'Administrator', fullName: 'Allen Walker', status: 'Active', language: 'Bilingual', license: 'G2',
-  // }, {
-  //   id: 1, userName: 'llee', type: 'Instructor', fullName: 'Lenalee Lee', status: 'Active', language: 'English', license: 'G',
-  // }, {
-  //   id: 2, userName: 'ykanda', type: 'Super Administrator', fullName: 'Yu Kanda', status: 'Active', language: 'French', license: 'None',
-  // },
+let users = [
+
 ];
+
+function onAfterInsertRow(row) {
+  UserService.createUser(row).then((returnValue) => {
+
+  }).catch((error) => {
+    // alert(error);
+  });
+}
+
+const options = {
+  afterInsertRow: onAfterInsertRow  // A hook for after insert rows
+};
+
+function onAfterSaveCell(row, cellName, cellValue) {
+  UserService.updateUser(row).then((returnValue) => {
+    if (!returnValue) {
+      alert('Error while updating');
+    }
+  }).catch((error) => {
+    alert(error);
+  });
+}
+
+function onBeforeSaveCell(row, cellName, cellValue) {
+  // You can do any validation on here for editing value,
+  // return false for reject the editing
+  if (!cellValue) {
+    return false;
+  }
+
+  return true;
+}
 
 const cellEditProp = {
   mode: 'click',
   blurToSave: true,
+  beforeSaveCell: onBeforeSaveCell, // a hook for before saving cell
+  afterSaveCell: onAfterSaveCell, // a hook for after saving cell
 };
 
-function onAfterInsertRow(row) {
-  // let newRowStr = '';
+function stringValidator(value) {
+  const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
+  if (!value) {
+    response.isValid = false;
+    response.notification.type = 'error';
+    response.notification.msg = 'Value must be inserted';
+    response.notification.title = 'Requested Value';
+  }
 
-  // for (const prop in row) {
-  //     newRowStr += prop + ': ' + row[prop] + ' \n';
-  // }
-  // console.log('The new row is:\n ' + newRowStr);
-  // console.log(row);
-    UserService.createUser(row).then((returnValue) => {
-      // users = returnedUsers;
-      // this.filterUsers();
-    }).catch((error) => {
-      alert(error);
-    });
+  return response;
 }
 
-const options = {
-  afterInsertRow: onAfterInsertRow   // A hook for after insert rows
-};
+function emptyValidator(value) {
+  const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
+  if (!value) {
+    response.isValid = false;
+    response.notification.type = 'error';
+    response.notification.msg = 'Value must be inserted';
+    response.notification.title = 'Requested Value';
+  }
 
+  return response;
+}
+
+function emailValidator(value) {
+  const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var emailIsValid = re.test(String(value).toLowerCase());
+
+  if (!value) {
+    response.isValid = false;
+    response.notification.type = 'error';
+    response.notification.msg = 'Value must be inserted';
+    response.notification.title = 'Requested Value';
+  } else if (!emailIsValid) {
+    response.isValid = false;
+    response.notification.type = 'error';
+    response.notification.msg = 'Value must be an email';
+    response.notification.title = 'Invalid Value';
+  }
+
+  return response;
+}
+
+function phoneValidator(value) {
+  const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
+  var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  var phoneIsValid = re.test(String(value).toLowerCase());
+
+  if (!value) {
+    response.isValid = false;
+    response.notification.type = 'error';
+    response.notification.msg = 'Value must be inserted';
+    response.notification.title = 'Requested Value';
+  } else if (!phoneIsValid) {
+    response.isValid = false;
+    response.notification.type = 'error';
+    response.notification.msg = 'Value must be a phone number';
+    response.notification.title = 'Invalid Value';
+  }
+
+  return response;
+}
 
 export default class UsersPage extends Component {
   constructor(props) {
@@ -59,19 +130,19 @@ export default class UsersPage extends Component {
       users = returnedUsers;
       this.filterUsers();
     }).catch((error) => {
-      alert(error);
+      // alert(error);
     });
   }
 
   filterUsers() {
     this.filteredUsers = [];
-
     for (let i = 0; i < users.length; i++) {
-      const status = users[i].status.toLowerCase();
-      if (this.state.showInactive || status === 'active') {
-        this.filteredUsers.push(users[i]);
-      }
+      // const status = users[i].status.toLowerCase();
+      // if (this.state.showInactive || status === 'active') {
+      this.filteredUsers.push(users[i]);
+      // }
     }
+
     this.setState({ filteredUsers: this.filteredUsers });
   }
 
@@ -107,18 +178,18 @@ export default class UsersPage extends Component {
           insertRow
           cellEdit={cellEditProp}
           search
-          options={ options }
+          options={options}
         >
           <TableHeaderColumn dataField="userId" isKey hidden hiddenOnInsert searchable={false} autoValue>Idb</TableHeaderColumn>
-          <TableHeaderColumn dataField="username" dataSort>User Name</TableHeaderColumn>
-          <TableHeaderColumn dataField="password" dataSort hidden searchable={false}>Password</TableHeaderColumn>
-          <TableHeaderColumn dataField="firstName" dataSort>Surname</TableHeaderColumn>
-          <TableHeaderColumn dataField="lastName" dataSort>Given Name</TableHeaderColumn>
-          <TableHeaderColumn dataField="email" dataSort>Email</TableHeaderColumn>
-          <TableHeaderColumn dataField="telephone" dataSort>Telephone</TableHeaderColumn>
-          <TableHeaderColumn dataField="dateOfBirth" dataSort>Date of Birth</TableHeaderColumn>
+          <TableHeaderColumn dataField="username" dataSort editable={{ validator: emptyValidator }}>User Name</TableHeaderColumn>
+          <TableHeaderColumn dataField="password" dataSort hidden searchable={false} editable={{ validator: emptyValidator }}>Password</TableHeaderColumn>
+          <TableHeaderColumn dataField="firstName" dataSort editable={{ validator: stringValidator }}>Surname</TableHeaderColumn>
+          <TableHeaderColumn dataField="lastName" dataSort editable={{ validator: stringValidator }}>Given Name</TableHeaderColumn>
+          <TableHeaderColumn dataField="email" dataSort editable={{ validator: emailValidator }}>Email</TableHeaderColumn>
+          <TableHeaderColumn dataField="telephone" dataSort editable={{ validator: phoneValidator }}>Telephone</TableHeaderColumn>
+          <TableHeaderColumn dataField="dateOfBirth" dataSort editable={{ validator: emptyValidator }}>Date of Birth</TableHeaderColumn>
           <TableHeaderColumn dataField="userType" dataSort editable={{ type: 'select', options: { values: userTypes } }}>User Type</TableHeaderColumn>
-          <TableHeaderColumn dataField="language" dataSort editable={{ type: 'select', options: { values: Languages } }}>Language(s)</TableHeaderColumn>
+          <TableHeaderColumn dataField="language" dataSort editable={{ name: 'language', type: 'select', options: { values: Languages } }}>Language(s)</TableHeaderColumn>
           <TableHeaderColumn dataField="driversLicense" dataSort editable={{ type: 'select', options: { values: licenseTypes } }}>Driver License</TableHeaderColumn>
           <TableHeaderColumn dataField="status" dataSort editable={{ type: 'select', options: { values: statusTypes } }}>Status</TableHeaderColumn>
         </BootstrapTable>
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,26 +1,41 @@
-/* global localStorage */
 import BaseService from './BaseService';
 
 export default class UserService extends BaseService {
-  
-  static getAllUsers(){
+  static getAllUsers() {
     return super.get('users/getallusers', null).then(res => res.data);
   }
 
-    static createUser(user){
-    return super.put('users/createuser', {
-		userId: user.userId,
-		firstName: user.FirstName,
-		lastName: user.lastName, 
-		username: user.username,
-		email: user.email,
-		telephone: user.telephone,
-		dateOfBirth: user.dateOfBirth,
-		userType: user.userType,
-		languages: user.languages,
-		driversLicense: user.driversLicense,
-		password: user.password, 
-		status: user.status,
+  static createUser(user) {
+    return super.post('users/createuser', {
+      userId: user.userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username,
+      email: user.email,
+      telephone: user.telephone,
+      dateOfBirth: user.dateOfBirth,
+      userType: user.userType,
+      language: user.language,
+      driversLicense: user.driversLicense,
+      password: user.password,
+      status: user.status,
+    }).then(res => res.data);
+  }
+
+  static updateUser(user) {
+    return super.put('users/updateuser', {
+      userId: user.userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username,
+      email: user.email,
+      telephone: user.telephone,
+      dateOfBirth: user.dateOfBirth,
+      userType: user.userType,
+      language: user.language,
+      driversLicense: user.driversLicense,
+      password: user.password,
+      status: user.status,
     }).then(res => res.data);
   }
 }
