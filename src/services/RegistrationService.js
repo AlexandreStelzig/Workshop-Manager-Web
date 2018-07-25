74b8c14b5a0870d92919d501e19fa0199098195d
Wendy <wgaul102@uottawa.ca>
2018-07-24 20:03:54 -0400

    Add fullname

--- src/services/RegistrationService.js
+++ src/services/RegistrationService.js
@@ -4,4 +4,8 @@ export default class RegistrationService extends BaseService {
   static submitRegistration(registrationValues) {
     return super.post('registration/submit', registrationValues).then(res => res.data);
   }
+
+  static getRegistrations() {
+    return super.get('registration/').then(res => res.data);
+  }
 }
